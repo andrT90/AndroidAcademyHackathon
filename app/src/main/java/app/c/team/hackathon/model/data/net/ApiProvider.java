aa2710014478d8ec7cdf7b7796b18fc382001e22
Andrey Tyugashev <atyugashev@stdev.ru>
2018-12-15 20:53:24 +0300

    Fonts. Api provider di.

--- app/src/main/java/app/c/team/hackathon/model/data/net/ApiProvider.java
+++ app/src/main/java/app/c/team/hackathon/model/data/net/ApiProvider.java
@@ -1,4 +1,12 @@
 package app.c.team.hackathon.model.data.net;
 
+import javax.inject.Inject;
+
 public class ApiProvider {
+    private final AndroidAcademyApi api;
+
+    @Inject
+    public ApiProvider(AndroidAcademyApi api) {
+        this.api = api;
+    }
 }
