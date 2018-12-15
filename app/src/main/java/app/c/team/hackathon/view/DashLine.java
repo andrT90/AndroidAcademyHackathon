e9add8afd47f05f05964f4a38f86575118d22ff1
Andrey Tyugashev <atyugashev@stdev.ru>
2018-12-16 02:44:56 +0300

    Dash line view. Refactor layout.

---
+++ app/src/main/java/app/c/team/hackathon/view/DashLine.java
@@ -0,0 +1,77 @@
+package app.c.team.hackathon.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import app.c.team.hackathon.R;
+
+
+public class DashLine extends View {
+    private Paint paint;
+    private int color;
+    private int dashLength;
+    private int dashGap;
+    private int dashStrokeWidth;
+    private boolean isHorizontal;
+
+    public DashLine(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public DashLine(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public DashLine(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    public DashLine(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DashLine, 0, 0);
+
+            try {
+                dashGap = a.getDimensionPixelSize(R.styleable.DashLine_dl_dashGap, 1);
+                dashLength = a.getDimensionPixelSize(R.styleable.DashLine_dl_dashLength, 1);
+                dashStrokeWidth = a.getDimensionPixelSize(R.styleable.DashLine_dl_dashStrokeWidth, 1);
+                color = a.getColor(R.styleable.DashLine_dl_color, 0xff000000);
+                isHorizontal = a.getBoolean(R.styleable.DashLine_dl_horizontal, false);
+            } finally {
+                a.recycle();
+            }
+        }
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(color);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(dashStrokeWidth);
+        paint.setPathEffect(new DashPathEffect(new float[]{dashLength, dashGap,}, 0));
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (isHorizontal) {
+            float center = getHeight() * .5f;
+            canvas.drawLine(0, center, getWidth(), center, paint);
+        } else {
+            float center = getWidth() * .5f;
+            canvas.drawLine(center, 0, center, getHeight(), paint);
+        }
+    }
+}
