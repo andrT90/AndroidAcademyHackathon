95577bcf0ef5a0a5b9b410f68c7a7ed177e12634
Andrey Tyugashev <atyugashev@stdev.ru>
2018-12-16 03:09:27 +0300

    Events repository in presenter.

--- app/src/main/java/app/c/team/hackathon/presentation/eventlist/EventListPresenter.java
+++ app/src/main/java/app/c/team/hackathon/presentation/eventlist/EventListPresenter.java
@@ -4,11 +4,25 @@ import android.support.annotation.Nullable;
 
 import com.arellomobile.mvp.InjectViewState;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import app.c.team.hackathon.model.data.AppSchedulers;
+import app.c.team.hackathon.model.domain.Event;
 import app.c.team.hackathon.presentation.base.BasePresenter;
+import app.c.team.hackathon.repository.EventRepository;
 import ru.terrakok.cicerone.Router;
 
 @InjectViewState
 public class EventListPresenter extends BasePresenter<EventListView> {
+
+    @Inject
+    EventRepository eventRepository;
+
+    @Inject
+    AppSchedulers schedulers;
 
     public EventListPresenter(@Nullable Router mainRouter, @Nullable Router localRouter) {
         super(mainRouter, localRouter);
@@ -17,5 +31,23 @@ public class EventListPresenter extends BasePresenter<EventListView> {
     @Override
     protected void onFirstViewAttach() {
         super.onFirstViewAttach();
+        addToComposite(
+                eventRepository
+                        .getEventList()
+                        .map(list -> {
+                            int count = list.size();
+                            List<Object> objects = new ArrayList<>(count);
+                            for (int i = 0; i < list.size(); i++) {
+                                Event event = list.get(i);
+                                objects.add(new EventItem(event, i == count - 1, i == 0));
+                            }
+                            return objects;
+                        })
+                        .observeOn(schedulers.main())
+                        .subscribe(
+                                list -> getViewState().showData(list),
+                                throwable -> getViewState().showError(throwable.getMessage())
+                        )
+        );
     }
 }
