a9444322dceb78a1325689d8ea5022efeb6086ee
achebotar <a.chebotar@um-agency.com>
2018-12-16 06:00:58 +0300

    bind views info screen

--- app/src/main/java/app/c/team/hackathon/presentation/info/EventInfoFragment.java
+++ app/src/main/java/app/c/team/hackathon/presentation/info/EventInfoFragment.java
@@ -2,10 +2,12 @@ package app.c.team.hackathon.presentation.info;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.design.widget.FloatingActionButton;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
@@ -14,14 +16,40 @@ import app.c.team.hackathon.R;
 import app.c.team.hackathon.presentation.base.BackButtonListener;
 import app.c.team.hackathon.presentation.base.BaseFragment;
 import app.c.team.hackathon.presentation.bottom.BottomNavView;
-import app.c.team.hackathon.presentation.event.EventPresenter;
-import app.c.team.hackathon.presentation.event.EventView;
 import butterknife.BindView;
 
 public class EventInfoFragment extends BaseFragment implements BackButtonListener, EventInfoView {
 
     @InjectPresenter
     EventInfoPresenter presenter;
+    @BindView(R.id.qr_container)
+    FrameLayout qrContainer;
+    @BindView(R.id.mentor_photo)
+    ImageView mentorPhoto;
+    @BindView(R.id.status)
+    ImageView status;
+    @BindView(R.id.mentor_name)
+    TextView mentorName;
+    @BindView(R.id.event_title)
+    TextView eventTitle;
+    @BindView(R.id.add_to_calendar_button)
+    LinearLayout addToCalendarButton;
+    @BindView(R.id.date)
+    TextView date;
+    @BindView(R.id.time)
+    TextView time;
+    @BindView(R.id.description)
+    TextView description;
+    @BindView(R.id.location)
+    Button location;
+    @BindView(R.id.wifi_name)
+    TextView wifiName;
+    @BindView(R.id.wifi_login)
+    TextView wifiLogin;
+    @BindView(R.id.wifi_password)
+    TextView wifiPassword;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @ProvidePresenter
     EventInfoPresenter provideTutorialPresenter() {
@@ -35,9 +63,7 @@ public class EventInfoFragment extends BaseFragment implements BackButtonListene
 
 
     public static EventInfoFragment newInstance() {
-
         Bundle args = new Bundle();
-
         EventInfoFragment fragment = new EventInfoFragment();
         fragment.setArguments(args);
         return fragment;
